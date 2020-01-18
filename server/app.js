a74df67988db8e7f3a73bd3419b92e0f12b8dbda
francesco <francesco.saverio.zuppichini@ch.pwc.com>
2020-01-18 16:04:46 +0100

    done

---
+++ server/app.js
@@ -0,0 +1,22 @@
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
+
+const port = process.env.PORT || "8080"
+
+var app = express()
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" })
+})
+
+app.listen(port, function() {
+  console.log(`Example app listening on port ${port}!`)
+})
